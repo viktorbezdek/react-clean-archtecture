aa2e1188177b5bd7b81bf34de4bc7a98849f0cbd
viktorbezdek <viktor.bezdek@gmail.com>
2021-05-06 19:15:24 +0000

    Deploying to gh-pages from @ viktorbezdek/react-clean-archtecture@8b791b798b8d24c3d29e4abb80afea2d87bbe259 🚀

--- README.md
+++
@@ -1,1 +0,0 @@
-preview available on
--- src/components/state/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Loading = () => {
-  return (
-    <div className='w-full h-10 text-xl text-center text-gray-300'>
-      Loading...
-    </div>
-  )
-}
